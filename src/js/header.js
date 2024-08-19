15049b15479fa8fc0ce560ef95c2d54e9db94ab9
Morg9864 <morgan.phemba@gmail.com>
2024-08-20 00:15:10 +0200

    refactor: Improve toggle functionality in header.js

--- src/js/header.js
+++ src/js/header.js
@@ -1,16 +1,18 @@
 document.getElementById("toggleButton").addEventListener("click", function () {
-	var moreText = document.getElementById("moreText");
+	var moreText = document.querySelectorAll(".moreText");
 	var icon = document.getElementById("toggleIcon");
 	var button = document.getElementById("toggleButton");
 
-	if (moreText.classList.contains("hidden")) {
-		moreText.classList.remove("hidden");
-		// Ajouter la classe qui fait pivoter la flèche vers le haut
-		button.classList.add("rotate-180");
-	} else {
-		moreText.classList.add("hidden");
-		// Retirer la classe pour remettre la flèche vers le bas
-		button.classList.remove("rotate-180");
+	if (moreText.length > 0) {
+		moreText.forEach(function (text) {
+			if (text.classList.contains("hidden")) {
+				text.classList.remove("hidden");
+				button.classList.add("rotate-180");
+			} else {
+				text.classList.add("hidden");
+				button.classList.remove("rotate-180");
+			}
+		});
 	}
 });
 
