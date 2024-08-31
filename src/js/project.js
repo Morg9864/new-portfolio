4c9b127803b170d26c41e18c6f0bd9d0b7b9c14c
Morg9864 <morgan.phemba@gmail.com>
2024-09-01 00:06:48 +0200

    refactor: Update project.js to dynamically load project data

---
+++ src/js/project.js
@@ -0,0 +1,40 @@
+// Récupérer l'ID du projet à partir de l'URL
+const urlParams = new URLSearchParams(window.location.search);
+const projectId = urlParams.get("id");
+
+// Charger les données du projet depuis un fichier JSON
+fetch("./src/data/projets.json")
+	.then((response) => response.json())
+	.then((data) => {
+		// Trouver le projet correspondant à l'ID
+		const projectData = data.projects.find(
+			(project) => project.id == projectId
+		);
+
+		if (projectData) {
+			// Mettre à jour les éléments de la page avec les données du projet
+			document.getElementById("page-title").textContent = `${projectData.title} - Projet`;
+			document.getElementById("project-title").textContent =
+				projectData.title;
+			document.getElementById("project-description").textContent =
+				projectData.description;
+			document.getElementById("project-image").src = projectData.image;
+			document.getElementById("project-link").href = projectData.link;
+			document.getElementById("project-github").href = projectData.github;
+
+			const technologiesList = document.getElementById(
+				"project-technologies"
+			);
+			projectData.technologies.forEach((tech) => {
+				const listItem = document.createElement("li");
+				listItem.textContent = tech;
+				technologiesList.appendChild(listItem);
+			});
+
+		} else {
+			console.error("Projet non trouvé !");
+		}
+	})
+	.catch((error) => {
+		console.error("Erreur lors du chargement des données:", error);
+	});
